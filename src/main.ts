6f4e8896f02e1749931ff8fa5f54c5e200bece3e
Rathma <rathma@kyso.dev>
2022-02-13 19:30:49 -0500

    Configured main.ts and vite.config.ts for Quasar, as per the documentation: https://quasar.dev/start/vite-plugin

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+	.use(Quasar, {
+		plugins: {} // import Quasar plugins and add here.
+	})
+	.mount('#app')
